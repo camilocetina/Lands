41143e0c9a06d36da03d4ea372561a9a1b2dff3b
camilocetina <64600635+camilocetina@users.noreply.github.com>
2020-05-21 18:23:34 -0500

    primer cambio con interfaz en xamarin

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Lands
+{
+    using views;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    public partial class App : Application
+    {
+        #region Contructor
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage( new LoginPage());
+        }
+
+        #endregion
+        #region metodos
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        } 
+        #endregion
+    }
+}
